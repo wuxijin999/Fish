23238545bb9bf1d1469bfa75dc7b12cbb0ec72cf
leonard Wu <364452445@qq.com>
2018-07-10 16:39:44 +0800

    更新。

---
+++ Assets/Scripts/Utility/Clock.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock
+{
+    Action onAlarm;
+    public readonly DateTime endTime;
+
+    public Clock(DateTime _endTime, Action _callBack)
+    {
+        endTime = _endTime;
+        onAlarm = _callBack;
+    }
+
+    public void Alarm()
+    {
+        if (onAlarm != null)
+        {
+            onAlarm();
+            onAlarm = null;
+        }
+    }
+
+    static bool inited = false;
+    static List<Clock> clocks = new List<Clock>();
+
+    public static void Create(DateTime _endTime, Action _callBack)
+    {
+        if (!inited)
+        {
+            GlobalTimeEvent.Instance.secondEvent += OnPerSecond;
+            inited = true;
+        }
+
+        clocks.Add(new Clock(_endTime, _callBack));
+    }
+
+    public static void Clear()
+    {
+        clocks.Clear();
+    }
+
+    public static void Dispose()
+    {
+        clocks.Clear();
+        inited = false;
+        GlobalTimeEvent.Instance.secondEvent -= OnPerSecond;
+    }
+
+    private static void OnPerSecond()
+    {
+        for (int i = clocks.Count - 1; i >= 0; i--)
+        {
+            var clock = clocks[i];
+            if (DateTime.Now > clock.endTime)
+            {
+                clock.Alarm();
+                clocks.Remove(clock);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Utility/GlobalTimeEvent.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GlobalTimeEvent : SingletonMonobehaviour<GlobalTimeEvent>
+{
+    public event Action secondEvent;
+    public event Action minuteEvent;
+    public event Action fiveMinuteEvent;
+    public event Action tenMinuteEvent;
+    public event Action halfHourEvent;
+    public event Action hourEvent;
+
+    int secondBuf = -1;
+    int minuteBuf = -1;
+    int fiveMinuteBuf = -1;
+    int tenMinuteBuf = -1;
+    int halfHourBuf = -1;
+    int hourBuf = -1;
+
+    public void Begin()
+    {
+
+    }
+
+    private void Update()
+    {
+        var second = DateTime.Now.Second;
+        if (second != secondBuf)
+        {
+            if (secondEvent != null)
+            {
+                secondEvent();
+            }
+
+            secondBuf = second;
+        }
+
+        var minute = DateTime.Now.Minute;
+        if (minuteBuf != minute)
+        {
+            try
+            {
+                if (minuteEvent != null)
+                {
+                    minuteEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                minuteBuf = minute;
+            }
+        }
+
+        var fiveMinute = minute / 5;
+        if (fiveMinuteBuf != fiveMinute)
+        {
+            try
+            {
+                if (fiveMinuteEvent != null)
+                {
+                    fiveMinuteEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                fiveMinuteBuf = fiveMinute;
+            }
+        }
+
+        var tenMinute = minute / 10;
+        if (tenMinuteBuf != tenMinute)
+        {
+            try
+            {
+                if (tenMinuteEvent != null)
+                {
+                    tenMinuteEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                tenMinuteBuf = tenMinute;
+            }
+        }
+
+        var thirtyMinute = minute / 30;
+        if (halfHourBuf != thirtyMinute)
+        {
+            try
+            {
+                if (halfHourEvent != null)
+                {
+                    halfHourEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                halfHourBuf = thirtyMinute;
+            }
+        }
+
+        var hour = DateTime.Now.Hour;
+        if (hourBuf != hour)
+        {
+            try
+            {
+                if (hourEvent != null)
+                {
+                    hourEvent();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+            finally
+            {
+                hourBuf = hour;
+            }
+        }
+
+    }
+
+
+}
