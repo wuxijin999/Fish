5fa1eddd4d55200477ead92045f18ac038372b87
leonard Wu <364452445@qq.com>
2018-07-24 11:03:24 +0800

    更新

---
+++ Assets/Scripts/UIComponent/TestBiz.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class TestBiz
+{
+
+    public JobProperty<string> testString1 = new JobProperty<string>();
+    public JobProperty<int> testInt1 = new JobProperty<int>();
+
+
+
+    public void UpdateString()
+    {
+        ThreadPool.QueueUserWorkItem(
+    (object aaa) =>
+    {
+        var index = 0;
+        while (index < 10)
+        {
+            testString1.value += "aaa ";
+            testString1.dirty = true;
+            index++;
+            Thread.Sleep(1000);
+        }
+    }
+    );
+    }
+
+    public void UpdateInt()
+    {
+        ThreadPool.QueueUserWorkItem(
+            (object aaa) =>
+            {
+                var index = 0;
+                while (index < 10)
+                {
+                    testInt1.value += 10;
+                    testInt1.dirty = true;
+                    index++;
+                    Thread.Sleep(1000);
+                }
+            }
+            );
+    }
+
+
+}
