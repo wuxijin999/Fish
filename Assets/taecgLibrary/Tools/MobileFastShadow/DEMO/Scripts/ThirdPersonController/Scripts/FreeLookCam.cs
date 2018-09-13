9860ffc5a3f1b96392f2c152793d0a2ebe412d6b
client_Wu Xijin <364452445@qq.com>
2018-09-13 09:58:59 +0800

    添加A+ Asset 资源管理插件

---
+++ Assets/taecgLibrary/Tools/MobileFastShadow/DEMO/Scripts/ThirdPersonController/Scripts/FreeLookCam.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+
+namespace taecg.tools.thirdPersonController
+{
+    //public class FreeLookCam : PivotBasedCameraRig
+    public class FreeLookCam : MonoBehaviour
+    {
+        [Header("跟随的目标")]
+        public Transform Target;
+        [Header("摄像机跟随目标的速度")]
+        public float FollowSpeed = 5f;
+        [Range(0f, 10f)] [SerializeField] private float m_TurnSpeed = 1.5f; 
+        [SerializeField] private float m_TurnSmoothing = 0.0f;                // How much smoothing to apply to the turn input, to reduce mouse-turn jerkiness
+        [Header("最大俯视角度")]
+        public float TiltMax = 80f;
+        [Header("最大仰视角度")]
+        public float TiltMin = 45f;
+       
+        private Transform m_Cam;
+        private Transform m_Pivot;
+        private float m_LookAngle;                    // The rig's y axis rotation.
+        private float m_TiltAngle;                    // The pivot's x axis rotation.
+        private const float k_LookDistance = 100f;    // How far in front of the pivot the character's look target is.
+		private Vector3 m_PivotEulers;
+		private Quaternion m_PivotTargetRot;
+		private Quaternion m_TransformTargetRot;
+
+        void Awake()
+        {
+            m_Cam = GetComponentInChildren<Camera>().transform;
+            m_Pivot = m_Cam.parent;
+			m_PivotEulers = m_Pivot.rotation.eulerAngles;
+
+	        m_PivotTargetRot = m_Pivot.transform.localRotation;
+			m_TransformTargetRot = transform.localRotation;
+        }
+
+
+        protected void Update()
+        {
+            HandleRotationMovement();
+
+            FollowTarget(Time.deltaTime);
+        }
+
+
+        void FollowTarget(float deltaTime)
+        {
+            if (Target == null)
+                Debug.LogWarning("跟随目标为空！");
+
+            // Move the rig towards target position.
+            transform.position = Vector3.Lerp(transform.position, Target.position, deltaTime*FollowSpeed);
+        }
+
+
+        private void HandleRotationMovement()
+        {
+			if(Time.timeScale < float.Epsilon)
+			return;
+
+            // Read the user input
+            var x = Input.GetAxis("Mouse X");
+            var y = Input.GetAxis("Mouse Y");
+
+            // Adjust the look angle by an amount proportional to the turn speed and horizontal input.
+            m_LookAngle += x*m_TurnSpeed;
+
+            // Rotate the rig (the root object) around Y axis only:
+            m_TransformTargetRot = Quaternion.Euler(0f, m_LookAngle, 0f);
+
+            // on platforms with a mouse, we adjust the current angle based on Y mouse input and turn speed
+            m_TiltAngle -= y * m_TurnSpeed;
+            // and make sure the new value is within the tilt range
+            m_TiltAngle = Mathf.Clamp(m_TiltAngle, -TiltMin, TiltMax);
+
+            // Tilt input around X is applied to the pivot (the child of this object)
+            m_PivotTargetRot = Quaternion.Euler(m_TiltAngle, m_PivotEulers.y , m_PivotEulers.z);
+
+			if (m_TurnSmoothing > 0)
+			{
+				m_Pivot.localRotation = Quaternion.Slerp(m_Pivot.localRotation, m_PivotTargetRot, m_TurnSmoothing * Time.deltaTime);
+				transform.localRotation = Quaternion.Slerp(transform.localRotation, m_TransformTargetRot, m_TurnSmoothing * Time.deltaTime);
+			}
+			else
+			{
+				m_Pivot.localRotation = m_PivotTargetRot;
+				transform.localRotation = m_TransformTargetRot;
+			}
+        }
+    }
+}
---
+++ Assets/taecgLibrary/Tools/MobileFastShadow/DEMO/Scripts/ThirdPersonController/Scripts/ThirdPersonUserControl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace taecg.tools.thirdPersonController
+{
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class ThirdPersonUserControl : MonoBehaviour
+    {
+        private ThirdPersonCharacter m_Character;
+        private Transform m_CamTrans; 
+        private Vector3 m_CamForward; 
+        private Vector3 moveVec3;
+
+        
+        void Start()
+        {
+            if (Camera.main != null)
+                m_CamTrans = Camera.main.transform;
+            else
+                Debug.LogError("请设置主角的摄像机Tag为MainCamera");
+
+            m_Character = GetComponent<ThirdPersonCharacter>();
+        }
+
+
+        void FixedUpdate()
+        {
+            //读取输入设置
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (m_CamTrans != null)
+            {
+                m_CamForward = Vector3.Scale(m_CamTrans.forward, new Vector3(1, 0, 1)).normalized;
+                moveVec3 = v * m_CamForward + h * m_CamTrans.right;
+            }
+
+            m_Character.Move(moveVec3);
+        }
+    }
+}
