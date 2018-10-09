df6f54e4b48451a7c7146aa6ac4a2cd3a03d492e
client_Wu Xijin <364452445@qq.com>
2018-10-09 21:32:49 +0800

    修改地图加载

--- Assets/Scripts/Stage/StageUtil.cs
+++ Assets/Scripts/Stage/StageUtil.cs
@@ -1,8 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageUtil : Singleton<StageUtil>
 {
+
+    const int LOGIN_STAGEID = 1;
+    const int CREATEROLE_STAGEID = 2;
+    const int SELECTROLE_STAGEID = 3;
+
+    public void LoadLogin()
+    {
+
+    }
+
+    public void LoadCreateRole()
+    {
+
+    }
+
+
+    public void LoadSelectRole()
+    {
+
+    }
+
+    public void Load<T>(int id) where T : Stage
+    {
+
+        var name = string.Empty;
+        CoroutineUtil.Instance.Begin(Co_Load<T>(name));
+    }
+
+
+    IEnumerator Co_Load<T>(string sceneName)
+    {
+        SceneManager.LoadScene("Empty");
+
+        var async = SceneManager.LoadSceneAsync(sceneName);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+
+
+
 
 }
