e3a3072e53035c50b4d9bf6a7abed30779400c50
client_Wu Xijin <364452445@qq.com>
2018-10-10 18:01:44 +0800

    修改测试功能

--- Assets/Scripts/Core/Fish.cs
+++ Assets/Scripts/Core/Fish.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿//----------------------------------------------------------------------
+//   这是一个全局类型，整个项目中具有最高地位
+//   没有得到主程许可 , 不允许更改这个类型
+//----------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Fish
 {
@@ -23,7 +29,14 @@ public class Fish
     {
         if (events.ContainsKey(type))
         {
-            events[type].Invoke();
+            try
+            {
+                events[type].Invoke();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log(ex);
+            }
         }
     }
 
