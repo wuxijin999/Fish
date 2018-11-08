d89cc1897af4a9dabde278c08e378103508b59e3
client_Wu Xijin <364452445@qq.com>
2018-11-08 18:00:17 +0800

    update

--- Assets/Scripts/System/MainWin/MainWin.cs
+++ Assets/Scripts/System/MainWin/MainWin.cs
@@ -6,27 +6,21 @@ using UnityEngine.Jobs;
 public class MainWin : Window
 {
 
-    protected override void BindController()
-    {
-    }
-
-    protected override void SetListeners()
-    {
-    }
-
-    protected override void OnAfterClose()
-    {
-    }
-
-    protected override void OnAfterOpen()
-    {
-    }
-
     protected override void OnPreClose()
     {
+        SetWidgetActive<SkillWidget>(false);
+        SetWidgetActive<JoyStickWidget>(false);
     }
 
     protected override void OnPreOpen()
     {
     }
+
+    protected override void OnActived()
+    {
+        base.OnActived();
+        SetWidgetActive<SkillWidget>(true);
+        SetWidgetActive<JoyStickWidget>(true);
+    }
+
 }
